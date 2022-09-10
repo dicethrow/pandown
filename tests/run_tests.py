727ea44ce26a35b37e7ad9cd2e4f8c75862c7ee6
Ubuntu <ubuntu@doc-dev.lxd>
2022-09-10 23:55:59 +0000

    Now lists of arbitary depth can be used, and tests are found with glob, so don't need to be manually listed

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,6 +2,7 @@ import unittest
 import os
 from pandown import run_local_cmd, remove_generated_files
 from contextlib import contextmanager
+import glob
 
 # from https://stackoverflow.com/questions/299446/how-do-i-change-directory-back-to-my-original-working-directory-with-python
 @contextmanager
@@ -13,30 +14,24 @@ def cwd(path):
     finally:
         os.chdir(oldpwd)
 
-test_docs = [
-	"test__basic",
-	"test__basic_nested_parts",
-	"test__basic_nested_parts__level_neg_1",
-	"test__level_neg_1",
-]
-
 class test_runner(unittest.TestCase):
 	""" 
 	Note that the pdf/html stuff will delete each other, annoyingly. Useful behaviour but not during testing.
 	"""
 	def setUp(self):
 		# remove outputs from the test folders
-		for test in test_docs:
-			with cwd(f"tests/{test}"):
+		for test_dir in glob.glob("tests/test__*"):
+			with cwd(test_dir):
 				remove_generated_files(keep_filetypes=[])
 
 
 	def test_pdf(self):
-		for test in test_docs:
-			with cwd(f"tests/{test}"):
+		for test_dir in glob.glob("tests/test__*"):
+			with cwd(test_dir):
+				print(f"Running test {test_dir}:")
 				result, error = run_local_cmd("python3 doc/build.py pdf")
 
-				self.assertEqual(error, [])
+				self.assertEqual(error, [], msg = f"{result},{error}")
 				resultFound = False
 				for line in result:
 					if "Output written on doc/output/result.pdf" in line:
@@ -47,13 +42,12 @@ class test_runner(unittest.TestCase):
 
 
 	def test_html(self):
-		for test in test_docs:
-			with cwd(f"tests/{test}"):
+		for test_dir in glob.glob("tests/test__*"):
+			with cwd(test_dir):
+				print(f"Running test {test_dir}:")
 				result, error = run_local_cmd("python3 doc/build.py html")
 
-				if len(error) > 0:
-					print(error)
-					assert 0
+				self.assertEqual(error, [], msg = f"{result},{error}")
 
 				for line in result:
 					self.assertFalse("Filter returned error status" in line)
