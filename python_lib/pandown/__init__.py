c45d7ac4b1a2c45b08a8683825140a4291fd8486
dicethrow <13843062+dicethrow@users.noreply.github.com>
2024-09-12 13:07:45 +1000

    Now HTML and PDF can be generated from the same file, reducing code duplication

--- python_lib/pandown/__init__.py
+++ python_lib/pandown/__init__.py
@@ -1,7 +1,5 @@
 # from lxdev import RemoteClient, myRemoteException, LOGGER
 from .doc_resources import get_path_to_common_content
 from .common import run_local_cmd, clear_terminal, remove_generated_files, debug_elem
-from .build_default_pdf import build_default_pdf, test_rmii_sync
-from .build_default_html import build_default_html
-from .build_default_odt import build_default_odt
+from .build_default_doc import build_default_doc, test_rmii_sync
 from .my_logging import loggerClass
